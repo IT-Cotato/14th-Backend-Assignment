68507e0c3008ac06baafee59b5581aab303107f5
kangcheolung <skhu202114111@gmail.com>
2026-09-12 09:23:26 +0900

    chore: initial project setup

---
+++ src/test/java/com/cotato/_th_backend_assignment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato._th_backend_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
