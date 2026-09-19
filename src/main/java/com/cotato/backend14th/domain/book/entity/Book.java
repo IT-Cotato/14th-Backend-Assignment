1a074c417acaad001ec61ad8b9a2fcbe604c1bf5
hjyoon <hjyoon5790@gmail.com>
2026-09-19 21:33:59 +0900

    feat: .env 및 프로필 설정, Book Swagger 문서화

---
+++ src/main/java/com/cotato/backend14th/domain/book/entity/Book.java
@@ -0,0 +1,20 @@
+// Book.java — 실제 DB 테이블과 매핑되는 객체
+package com.cotato.backend14th.domain.book.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor // JPA는 기본 생성자가 필수
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 자동으로 id를 증가시켜줘요
+    private Long id;
+
+    private String title;
+    private String author;
+}
