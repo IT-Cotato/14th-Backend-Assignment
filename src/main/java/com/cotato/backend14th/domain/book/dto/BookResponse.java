eaaf9d0d45ea746fffd035d81c241b1754f65865
soohyungit <soohyun5576@gmail.com>
2026-09-21 16:08:26 +0900

    feat: .env 및 프로필 설정, Book Swagger 문서화

---
+++ src/main/java/com/cotato/backend14th/domain/book/dto/BookResponse.java
@@ -0,0 +1,12 @@
+package com.cotato.backend14th.domain.book.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor // 모든 필드를 받는 생성자를 자동으로 만들어줘요
+public class BookResponse {
+    private Long id;
+    private String title;
+    private String author;
+}
