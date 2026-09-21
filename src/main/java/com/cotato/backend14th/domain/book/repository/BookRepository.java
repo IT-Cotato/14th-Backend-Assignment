45a59371537b80c484cde2018a2d1aa3fd38096d
박현빈 <empty6004@kookmin.ac.kr>
2026-09-21 19:48:37 +0900

    feat: .env 및 프로필 설정, Book Swagger 문서화

---
+++ src/main/java/com/cotato/backend14th/domain/book/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.cotato.backend14th.domain.book.repository;
+
+import com.cotato.backend14th.domain.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    // save(), FindByID() 등 기본 CRUD 메서드는 JpaRepository가 자동으로 제공한다.
+}
